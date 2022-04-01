1518caa2104cd383930d691c429fd3d9e6cb6959
Maximilian Alba <albam476@gmail.com>
2022-04-01 16:47:03 -0400

    commit tree cb5977953e46373dfc77a28c3989b39c675fe991

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching (driverList, name){
+
+    return driverList.filter(element => element.toLowerCase() === name.toLowerCase())
+
+}
+
+function fuzzyMatch (driverList, name){
+    
+    return driverList.filter(element => element.startsWith(name))
+
+}
+
+function matchName (driverList, name){
+    
+    return driverList.filter(element => element.name.toLowerCase() === name.toLowerCase())
+
+}
